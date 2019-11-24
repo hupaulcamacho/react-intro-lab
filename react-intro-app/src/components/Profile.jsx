e686150f1ca5d6838b9ffd2e154ccb4290f0e8e4
Hupaul <hupaulcamacho@pursuit.org>
2019-11-23 22:35:56 -0500

    submitting lab

--- react-intro-app/src/components/Profile.jsx
+++ react-intro-app/src/components/Profile.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Profile.css'
+import profile from './male.png'
+
 class Profile extends React.Component {
+    profileInfo = {
+        name: 'Jordan Walke',
+        title: 'React Creator',
+        profileimg: profile,
+        bio: 'Lorem ipsum'
+    }
     render() {
         return (
             <div className='profile'>
-                <h2> Jordan Walke </h2>
-                <h3> React Creator </h3>
-                <p> Lorem ipsum </p>
+                <img src={this.profileInfo.profileimg} width='150' height='150'></img>
+                <div className='profileinfo'>
+                    <h3>{this.profileInfo.name}</h3>
+                    <h4>{this.profileInfo.title}</h4>
+                    <p> {this.profileInfo.bio}</p>
+                </div>
+                
             </div>
         )
     }
